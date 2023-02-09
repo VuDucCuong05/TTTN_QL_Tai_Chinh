6f7aacd16650bc18ba41b17f14bca9699337239a
VuDucCuong05 <vuduccuong150301@gmail.com>
2023-02-09 17:20:43 +0700

    tạo chức năng nhập tiêu dùng, thu nhập

--- app/src/main/java/com/example/qltaichinhcanhan/fragment/ReportFragment.kt
+++ app/src/main/java/com/example/qltaichinhcanhan/fragment/ReportFragment.kt
@@ -1,24 +1,34 @@
 package com.example.qltaichinhcanhan.fragment
 
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.DatePicker.OnDateChangedListener
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.qltaichinhcanhan.R
 import com.example.qltaichinhcanhan.adapter.AdapterMoney
 import com.example.qltaichinhcanhan.databinding.FragmentReportBinding
+import com.example.qltaichinhcanhan.mode.Money
 import com.example.qltaichinhcanhan.viewModel.MoneyViewModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class ReportFragment : Fragment() {
     private lateinit var binding: FragmentReportBinding
     private lateinit var moneyViewModel: MoneyViewModel
     private lateinit var adapterMoney: AdapterMoney
+
+    var d = Calendar.getInstance().time
+    private var date = d.date
+    private var month = d.month + 1
+    private var year = d.year + 1900
+
+
     fun newInstance(): ReportFragment {
         val args = Bundle()
         val fragment = ReportFragment()
@@ -35,23 +45,49 @@ class ReportFragment : Fragment() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         moneyViewModel = ViewModelProvider(this)[MoneyViewModel::class.java]
         initView()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun initView() {
         adapterMoney = AdapterMoney(requireContext(), listOf())
         binding.rcMoney.adapter = adapterMoney
         binding.rcMoney.layoutManager =
             LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
+        var arrayMoney = arrayListOf<Money>()
+
         activity?.let {
             moneyViewModel.readAllData.observe(it) {
-                adapterMoney.updateData(it)
+                arrayMoney = it as ArrayList<Money>
             }
         }
+
+        binding.datePicker.setOnDateChangedListener(OnDateChangedListener { datePicker, i, i1, i2 ->
+            print("date changed by user")
+            date = datePicker.dayOfMonth
+            month = datePicker.month + 1
+            year = datePicker.year
+            adapterMoney.updateData(checkMoney(date, month, year, arrayMoney))
+        })
     }
 
+    private fun checkMoney(
+        day: Int,
+        month: Int,
+        year: Int,
+        arrayMoney: ArrayList<Money>,
+    ): ArrayList<Money> {
+        var newArrayListMoney = arrayListOf<Money>()
+        for (i in arrayMoney) {
+            if (month == i.month && year == i.year && day == i.day) {
+                newArrayListMoney.add(i)
+            }
+        }
+        return newArrayListMoney
+    }
 }
--- app/src/main/java/com/example/qltaichinhcanhan/fragment/SummaryFragment.kt
+++ app/src/main/java/com/example/qltaichinhcanhan/fragment/SummaryFragment.kt
@@ -1,21 +1,28 @@
 package com.example.qltaichinhcanhan.fragment
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.qltaichinhcanhan.R
-import com.example.qltaichinhcanhan.adapter.AdapterCategory
 import com.example.qltaichinhcanhan.adapter.AdapterMoney
-import com.example.qltaichinhcanhan.databinding.FragmentExpenseBinding
 import com.example.qltaichinhcanhan.databinding.FragmentSummaryBinding
 import com.example.qltaichinhcanhan.mode.Money
-import com.example.qltaichinhcanhan.viewModel.CategoryViewModel
 import com.example.qltaichinhcanhan.viewModel.MoneyViewModel
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.MPPointF
+import java.util.*
 
 class SummaryFragment : Fragment() {
     private lateinit var binding: FragmentSummaryBinding
@@ -25,6 +32,10 @@ class SummaryFragment : Fragment() {
 
     lateinit var arrayMoneyE: ArrayList<Money>
     lateinit var arrayMoneyI: ArrayList<Money>
+    lateinit var arrayMoney: ArrayList<Money>
+
+    var list: Vector<Money> = Vector<Money>()
+
     fun newInstance(): SummaryFragment {
         val args = Bundle()
         val fragment = SummaryFragment()
@@ -47,21 +58,32 @@ class SummaryFragment : Fragment() {
     }
 
     private fun initView() {
+        val myLinearLayoutManager = object : LinearLayoutManager(requireContext()) {
+            override fun canScrollVertically(): Boolean {
+                return false
+            }
+        }
+
         adapterMoneyE = AdapterMoney(requireContext(), listOf())
+        binding.rcvMoneyE.layoutManager = myLinearLayoutManager
         binding.rcvMoneyE.adapter = adapterMoneyE
-        binding.rcvMoneyE.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        val myLinearLayoutManager1 = object : LinearLayoutManager(requireContext()) {
+            override fun canScrollVertically(): Boolean {
+                return false
+            }
+        }
 
         adapterMoneyI = AdapterMoney(requireContext(), listOf())
-
+        binding.rcvMoneyI.layoutManager = myLinearLayoutManager1
         binding.rcvMoneyI.adapter = adapterMoneyI
-        binding.rcvMoneyI.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-
 
 
         activity?.let {
             moneyViewModel.readAllData.observe(it) {
+                arrayMoney = arrayListOf()
+                arrayMoney = it as ArrayList<Money>
+
                 arrayMoneyE = arrayListOf()
                 arrayMoneyI = arrayListOf()
                 for (i in it) {
@@ -71,10 +93,108 @@ class SummaryFragment : Fragment() {
                         arrayMoneyI.add(i)
                     }
                 }
-                adapterMoneyE.updateData(arrayMoneyE)
-                adapterMoneyI.updateData(arrayMoneyI)
+
             }
         }
+
+
+        binding.btnSummaryEnter.setOnClickListener {
+            val txtMonth = binding.etSummaryMonth.text.toString().toInt()
+            val txtYear = binding.etSummaryYear.text.toString().toInt()
+            var currency = 1
+            var currencyString = ""
+
+            when (binding.radioGroup.checkedRadioButtonId) {
+                R.id.rd_vnd -> {
+                    currency = 1
+                    currencyString = "VNĐ"
+                }
+                R.id.rd_usd -> {
+                    currency = 2
+                    currencyString = "USD"
+                }
+            }
+
+            if (txtMonth != 0 && txtYear != 0 && txtMonth > 0 && txtMonth < 12) {
+                val aE = checkMoney(currency, txtMonth, txtYear, arrayMoneyE)
+                val aI = checkMoney(currency, txtMonth, txtYear, arrayMoneyI)
+
+                if (aE.size != 0) {
+                    binding.rcvMoneyE.visibility = View.VISIBLE
+                    binding.textNullE.visibility = View.INVISIBLE
+                    adapterMoneyE.updateData(aE)
+                } else {
+                    binding.rcvMoneyE.visibility = View.INVISIBLE
+                    binding.textNullE.visibility = View.VISIBLE
+                }
+                if (aI.size != 0) {
+                    binding.rcvMoneyI.visibility = View.VISIBLE
+                    binding.textNullI.visibility = View.INVISIBLE
+                    adapterMoneyI.updateData(aI)
+                } else {
+                    binding.rcvMoneyI.visibility = View.INVISIBLE
+                    binding.textNullI.visibility = View.VISIBLE
+                }
+                if (aE.size != 0 || aI.size != 0) {
+                    binding.pieChart.visibility = View.VISIBLE
+                    addDataToPieChart(arrayMoney, currencyString)
+                } else {
+                    binding.pieChart.visibility = View.GONE
+                }
+            } else {
+                Toast.makeText(context,
+                    "Giá trị nhập vào không đúng định dạng. Vui lòng nhập lại!",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 
+    private fun checkMoney(
+        currency: Int,
+        month: Int,
+        year: Int,
+        arrayMoney: ArrayList<Money>,
+    ): ArrayList<Money> {
+        var newArrayListMoney = arrayListOf<Money>()
+        for (i in arrayMoney) {
+            if (month == i.month && year == i.year && currency == i.currency) {
+                newArrayListMoney.add(i)
+            }
+        }
+        return newArrayListMoney
+    }
+
+    private fun addDataToPieChart(arrayMoney: ArrayList<Money>, type: String) {
+        val val_entry = Vector<PieEntry>()
+        val colors = Vector<Int>()
+        var sumExpense = 0
+        var sumIncome = 0
+        for (i in arrayMoney) {
+            if (i.type == 1) {
+                sumExpense += i.amount!!
+            } else if (i.type == 2) {
+                sumIncome += i.amount!!
+            }
+        }
+        val_entry.add(PieEntry(sumExpense + 0.0F, 0))
+        val_entry.add(PieEntry(sumIncome + 0.0F, 1))
+
+
+        val pieDataSet = PieDataSet(val_entry, "Tiêu dùng và thu nhập")
+        pieDataSet.sliceSpace = 2f
+        pieDataSet.valueTextSize = 10f
+        colors.add(Color.rgb(26, 102, 255)) //Blue
+        colors.add(Color.rgb(225, 111, 84)) // Orange
+        pieDataSet.colors = colors
+        val description = Description()
+        description.text = "Đơn vị tiền tệ: $type"
+        val pieData = PieData(pieDataSet)
+        pieData.setValueTextColor(Color.WHITE)
+        binding.pieChart.data = pieData
+        binding.pieChart.description = description
+        binding.pieChart.invalidate()
+    }
+    // https://www.geeksforgeeks.org/android-create-a-pie-chart-with-kotlin/
 }
